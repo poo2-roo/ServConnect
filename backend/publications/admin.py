0851bceb0c6ec4c939b7c8e79bf4da9ef7541c52
poo2-roo <jasonngando46@gmail.com>
2026-09-05 03:15:43 +0200

    Enregistrer les modeles Publication et Commentaire dans l'admin Django

--- backend/publications/admin.py
+++ backend/publications/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Commentaire, Publication
 
-# Register your models here.
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('prestataire', 'type_publication', 'date_creation')
+    list_filter = ('type_publication',)
+    search_fields = ('contenu', 'prestataire__nom_entreprise')
+
+
+@admin.register(Commentaire)
+class CommentaireAdmin(admin.ModelAdmin):
+    list_display = ('publication', 'auteur', 'date_creation')
