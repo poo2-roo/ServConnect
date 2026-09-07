0e1213dfca9cfe4196c94bfabdc28c14a041772b
poo2-roo <jasonngando46@gmail.com>
2026-09-07 15:57:44 +0200

    Ajouter endpoint pour modifie

--- backend/messaging/serializers.py
+++ backend/messaging/serializers.py
@@ -18,13 +18,15 @@ class MessageSerializer(serializers.ModelSerializer):
 class ConversationSerializer(serializers.ModelSerializer):
     client_nom = serializers.CharField(source='client.utilisateur.get_full_name', read_only=True)
     prestataire_nom = serializers.CharField(source='prestataire.nom_entreprise', read_only=True)
+    client_avatar = serializers.SerializerMethodField()
+    prestataire_avatar = serializers.SerializerMethodField()
     dernier_message = serializers.SerializerMethodField()
     messages_non_lus = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = [
-            'id', 'client', 'client_nom', 'prestataire', 'prestataire_nom',
+            'id', 'client', 'client_nom', 'client_avatar', 'prestataire', 'prestataire_nom', 'prestataire_avatar',
             'service', 'date_creation', 'derniere_activite', 'dernier_message', 'messages_non_lus',
         ]
         read_only_fields = ['client', 'date_creation', 'derniere_activite']
@@ -32,6 +34,19 @@ class ConversationSerializer(serializers.ModelSerializer):
     def get_dernier_message(self, obj):
         dernier = obj.messages.order_by('-date_envoi').first()
         return MessageSerializer(dernier).data if dernier else None
+
+    def _get_avatar_url(self, utilisateur):
+        photo = utilisateur.photo_profil
+        if not photo:
+            return None
+        request = self.context.get('request')
+        return request.build_absolute_uri(photo.url) if request else photo.url
+
+    def get_client_avatar(self, obj):
+        return self._get_avatar_url(obj.client.utilisateur)
+
+    def get_prestataire_avatar(self, obj):
+        return self._get_avatar_url(obj.prestataire.utilisateur)
 
     def get_messages_non_lus(self, obj):
         request = self.context.get('request')
